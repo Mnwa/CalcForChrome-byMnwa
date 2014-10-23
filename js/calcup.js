756e2132d827d9ba40a10727d70fed2663254f11
Mnwa <mihan@panfilov.biz>
2014-10-24 00:08:34 +0400

    Revert "Revert "Up""
    This reverts commit 12604428c3adeb08ce97fcd60d2c84795c0339de.

--- js/calcup.js
+++ js/calcup.js
@@ -6,22 +6,24 @@ function check(text, val){
 		}
 	}
 	if(num.length == 0){
-		num.push(0);
-		res.push(b.substring(Math.max.apply(Math, num),  b.length));
+		res.push(b);
 	}
 	else{
 		res.push(b.substring(Math.max.apply(Math, num),  b.length));
 		}
 	num.length=0;
 	for(var i = 0; i!=e.length+1; i++){
-		if(e.substring(i-1,i) == '+' || e.substring(i-1,i) == '-' || e.substring(i-1,i) == '*' || e.substring(i-1,i) == '/' ){
+		if((e.substring(i-1,i) == '+' || e.substring(i-1,i) == '-' || e.substring(i-1,i) == '*' || e.substring(i-1,i) == '/') && i-1!=0 && i-1!=-1){
 			num.push(i-1);
 		}
 	}
+	
 	if(num.length == 0){
-		num.push(e.length);
+		res.push(e);
 	}
-	res.push(e.substring(0, Math.min.apply(Math, num)));
+	else{
+		res.push(e.substring(0, Math.min.apply(Math, num)));
+	}
 	return res
 }
 
@@ -37,13 +39,13 @@ function calc(text){
 		num = text.indexOf('^', num+1);
 	}
 	num=0;
-	num = text.indexOf('&');
+	num = text.indexOf('√');
 	while(num!=-1){
 		if(num != -1){
 			arr = check(text, num); 
-			text = text.replace('&'+arr[1], Math.sqrt(arr[1]));
+			text = text.replace('√'+arr[1], Math.sqrt(arr[1]));
 		}
-		num = text.indexOf('&', num+1);
+		num = text.indexOf('√', num+1);
 	}
 	num=0;
 	num = text.indexOf('*');
@@ -86,14 +88,32 @@ function calc(text){
 
 function main(text){
 	var num = 0, io = 0;
-	num = text.indexOf(')');
+	text = text.replace(/π/g,Math.PI);
+	text = text.replace(/e/g,Math.E);
+	num = text.indexOf('sin(');																					//sin	
 	while(num!=-1){
-		io = text.indexOf('(');
-		while(io < num && text.indexOf('(', io+1) != -1){
-			io = text.indexOf('(', io+1);
-		}
-		text = text.replace(text.substring(io, num+1), calc(text.substring(io+1, num)));
+		text = text.replace(/sin\([-0-9.+\/*√^]+\)/, Math.sin(calc(text.substring(num+4, text.indexOf(")",num)))));	
 		num = text.indexOf(')', num+1);
+	}
+	num = text.indexOf('cos(');																					//cos
+	while(num!=-1){
+		text = text.replace(/cos\([-0-9.+\/*√^]+\)/, Math.cos(calc(text.substring(num+4, text.indexOf(")",num)))));
+		num = text.indexOf(')', num+1);
+	}
+	num = text.indexOf('ctg(');																					//ctan
+	while(num!=-1){
+		text = text.replace(/ctg\([-0-9.+\/*√^]+\)/, 1/Math.tan(calc(text.substring(num+5, text.indexOf(")",num)))));
+		num = text.indexOf(')', num+1);
+	}
+	num = text.indexOf('tg(');																					//tan
+	while(num!=-1){
+		text = text.replace(/tg\([-0-9.+\/*√^]+\)/, Math.tan(calc(text.substring(num+4, text.indexOf(")",num)))));
+		num = text.indexOf(')', num+1);
+	}
+	num = text.indexOf('(');																					//(..)
+	while(num!=-1){
+		text = text.replace(/\([-0-9.+\/*√^]+\)/, calc(text.substring(num+1, text.indexOf(")",num))));
+		num = text.indexOf('(', num+1);
 	}
 	return calc(text)
 }
