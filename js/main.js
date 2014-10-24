be57f5f30db7626991cb0a20732e8b36640f8e08
Mnwa <mihan@panfilov.biz>
2014-10-24 18:01:18 +0400

    24.10.2014
    Add buttons Clean, Backspace

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 	$(document).ready(function(){
-		$('div#but input').click(function() {											//Ввод чисел
+		$('div#but input').click(function() {											//Input id=but
 			$('.textbox').val($('.textbox').val() + this.value.toString());
 		})
 		
-		$('div.vol input#sum').click(function() {											//Ввод знаков
+		$('div.vol input#sum').click(function() {										//Input id=sum
 			$('.textbox').val($('.textbox').val() + this.value.toString());
 		})
-		$('div.vol input#add').click(function() {			//Ввод
+		$('div.vol input#add').click(function() {										//Enter
 			$('.textbox').val(main($('.textbox').val()));
 		})
-		$('div.vol input#bck').click(function() {			//Backspace
+		$('div.vol input#bck').click(function() {										//Backspace
 			$('.textbox').val($('.textbox').val().slice(0,-1));
 		})
-		$('div.vol input#c').click(function() {			//Clean
+		$('div.vol input#c').click(function() {											//Clean
 			$('.textbox').val('');
 		})
 	});
