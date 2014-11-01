ad2b03ee659da7b3749c84c0550d58f9d999736b
Mnwa <mihan@panfilov.biz>
2014-11-02 02:10:37 +0300

    02.11.2014
    Replaced input on button

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
 	$(document).ready(function(){
-		$('div#but input').click(function() {											//Input id=but
+		$('button').width(30);
+		$('html').width(200);
+		$('html').height(300);
+		$('button').height(30);
+		$('button#but').click(function() {											//Input id=but
 			$('.textbox').val($('.textbox').val() + this.value.toString());
 		})
 		
-		$('div.vol input#sum').click(function() {										//Input id=sum
+		$('button#sum').click(function() {										//Input id=sum
 			$('.textbox').val($('.textbox').val() + this.value.toString());
 		})
-		$('div.vol input#add').click(function() {										//Enter
+		$('button#add').click(function() {										//Enter
 			$('.textbox').val(main($('.textbox').val()));
 		})
-		$('div.vol input#bck').click(function() {										//Backspace
+		$('button#bck').click(function() {										//Backspace
 			$('.textbox').val($('.textbox').val().slice(0,-1));
 		})
-		$('div.vol input#c').click(function() {											//Clean
+		$('button#c').click(function() {											//Clean
 			$('.textbox').val('');
 		})
 	});
+
